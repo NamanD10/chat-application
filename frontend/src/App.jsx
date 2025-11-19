2290e0921f2c9395cb6d7465eb48ccba63a48105
Naman Dubey <namandubey10@gmail.com>
2025-11-19 23:39:32 +0530

    Added video call feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -68,13 +68,25 @@ const App = () => {
         />
         
         <Route 
-          path='/call' 
-          element={isAuthenticated ? <CallPage /> : <Navigate to='/login' />} 
+          path='/call/:id' 
+          element={
+            isAuthenticated && isOnboarded ? (
+            <CallPage />
+          ) : (
+          <Navigate to={!isAuthenticated ? '/login' : '/onboarding'} />
+          )
+          }
         />
         
         <Route 
           path= '/chat/:id' 
-          element={isAuthenticated ? <ChatPage /> : <Navigate to='/login' />} 
+          element={
+            isAuthenticated && isOnboarded ? (
+            <ChatPage />
+          ) : (
+          <Navigate to={!isAuthenticated ? '/login' : '/onboarding'} />
+          )
+          } 
         />
         
        <Route
